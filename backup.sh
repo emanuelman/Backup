20a623e176aa63ae8768ede360ea140e575eed37
Emanuel Antunes <72533265+emanuelman@users.noreply.github.com>
2023-01-04 15:37:58 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#Esta funcao é pra ser utilizada antes de formatar o pc
 function compactar(){
 echo "
 ----------------------------
@@ -7,8 +8,18 @@ Compactando os Diretórios...
 ----------------------------
 "
 sleep 1s
-#cd ~
-#mkdir Backup
+
+caminho=~/Backup
+if [ ! -d "$caminho" ]; then
+    echo "O diretorio 'Backup' ainda não existe"
+    echo "Criando diretorio Backup..."
+    mkdir ~/Backup
+    #echo "Diretorio não existe!"
+else
+    echo "O diretorio 'Backup' já existe"
+    #echo "Diretorio existe"
+fi
+
 cd ~/Desktop
 zip Personal.zip -r Personal -6
 sudo mv Personal.zip ~/Backup
@@ -22,19 +33,27 @@ sudo zip Localhost.zip -r html -6
 sudo mv Localhost.zip ~/Backup
 }
 
-function organizar(){
+#Esta funcao é pra ser utilizada logo apos formatar o pc
+function descompactar(){
+#Personal
+sudo mv ~/Backup/Personal.zip ~/Desktop
+cd ~/Desktop
+unzip Personal
+#Documents, Videos e Pictures
 cd ~
 rm -rf Documents Videos Pictures
-sudo mv Personal.zip ~/Desktop
-unzip Personal
-sudo mv Documents.zip Videos.zip Pictures.zip ~
+sudo mv ~/Backup/Documents.zip ~/Backup/Videos.zip ~/Backup/Pictures.zip ~
 unzip Documents.zip Videos.zip Pictures.zip
-sudo mv Localhost.zip /var/www/
+#Localhost
+sudo mv ~/Backup/Localhost.zip /var/www/html
+cd /var/www/html
+sudo unzip Localhost.zip
+#Criar links simbolicos
 ln -s ~/Desktop/Personal/Calistenia calistenia
 ln -s Desktop/Personal/Projetos projetos
 ln -s ~/Desktop/Personal/Estudos estudos
-#ln -s /var/www/html localhost
+sudo ln -s /var/www/html localhost
 }
 
 compactar
-#organizar
+descompactar
